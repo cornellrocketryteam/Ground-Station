11189895b53a41e6f1fbe0a614800bd905900acd
Benjamin Zou <bwz5@cornell.edu>
2024-03-04 14:14:08 -0500

    finishing packing

--- src/dataprocessing.cpp
+++ src/dataprocessing.cpp
@@ -6,53 +6,16 @@
 
 std::unordered_map<std::string, float> SerialRead::serialValues;
 
-/**
- * 6 bytes: Metadata
-3 bits: flight mode
-10 bits: Sensor statuses (2 bits each)
-1 bit: SD state
-1 bit: key armed
-1 bit: altitude armed
-4 bytes: timestamp
-3 bytes: Events
-(Will come up with enumeration)
-52 bytes: Sensor readings (4 bytes each)
-Altitude
-Latitude
-Longitude
-Elevation
-Accel X
-Accel Y
-Accel Z
-Gyro X
-Gyro Y
-Gyro Z
-Mag X
-Mag Y
-Mag Z 
+// Initialize all sensors to working 
+WorkingState AltimeterState = WORKING;
+WorkingState GpsState = WORKING;
+WorkingState IMUState = WORKING;
+WorkingState SDCardState = WORKING;
+WorkingState AccelerometerState = WORKING; 
+WorkingState TemperatureState = WORKING; 
+WorkingState RadioState = WORKING;
 
-For the EVENTS:
-0: Key armed
-1: Altitude armed
-2: Alt init failed
-3: Alt reading failed
-4: Alt turned off
-5: IMU init failed
-6: IMU reading failed
-7: IMU turned off
-8: Accel init failed
-9: Accel reading failed
-10: Accel turned off
-11: Therm init failed
-12: Therm reading failed
-13: Therm turned off
-14: SD init failed
-15: SD write failed
-16: RFM init failed
-17: RFM transmit failed
-
-0 means false, 1 means true 
-*/
+FlightMode FlightState = StartupMode; 
 
 void SerialRead::readPack(){
     if (wiringPiSetup() == -1) {
@@ -70,18 +33,34 @@ void SerialRead::readPack(){
     u_int8_t metaData1 = digitalRead8(RX); 
     u_int8_t metaData2 = digitalRead8(RX); 
 
-    // Test the right-most bit of metaData1
     for (int i = 0; i < 8; i++){
         if (metaData1 & 0x01){
-            // rightmost bit is 1
+            switch(i){
+                case 0: // Flight Mode
+                case 1: // Flight Mode
+                case 2: // Flight Mode
+                case 3: // Do 3 -> 7 matter, because we are getting event data?
+                case 4:
+                case 5:
+                case 6:
+                case 7: 
+            }
         }
         metaData1 = metaData1 >> 1; 
     }
 
-    // Test the right-most bit of metaData2
     for (int i = 0; i < 8; i++){
         if (metaData2 & 0x01){
-            // rightmost bit is 1
+            switch(i){
+                case 0: 
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7: 
+            }
         }
         metaData2 = metaData2 >> 1; 
     }
@@ -96,7 +75,16 @@ void SerialRead::readPack(){
     // Test the right-most bit of Events1
     for (int i = 0; i < 8; i++){
         if (Events1 & 0x01){
-            // rightmost bit is 1
+            switch(i){
+                case 0: IMUState = FAILURE; 
+                case 1: IMUState = FAILURE; 
+                case 2: IMUState = FAILURE; 
+                case 3: AltimeterState = FAILURE; 
+                case 4: AltimeterState = FAILURE; 
+                case 5: AltimeterState = FAILURE; 
+                case 6: // When altitude is armed 
+                case 7: // When key is armed 
+            }
         }
         Events1 = Events1  >> 1; 
     }
@@ -104,7 +92,16 @@ void SerialRead::readPack(){
     // Test the right-most bit of Events2
     for (int i = 0; i < 8; i++){
         if (Events2 & 0x01){
-            // rightmost bit is 1
+            switch(i){
+                case 0: SDCardState = FAILURE; 
+                case 1: SDCardState = FAILURE; 
+                case 2: TemperatureState = FAILURE; 
+                case 3: TemperatureState = FAILURE; 
+                case 4: TemperatureState = FAILURE; 
+                case 5: AccelerometerState = FAILURE;
+                case 6: AccelerometerState = FAILURE; 
+                case 7: AccelerometerState = FAILURE;
+            }
         }
         Events2 = Events2 >> 1; 
     }
@@ -112,7 +109,11 @@ void SerialRead::readPack(){
     // Test the right-most bit of Events3
     for (int i = 0; i < 8; i++){
         if (Events3 & 0x01){
-            // rightmost bit is 1
+            switch(i){
+                // 0 through 5 are unimportant 
+                case 6: RadioState = FAILURE; 
+                case 7: RadioState = FAILURE; 
+            }
         }
         Events3 = Events3 >> 1; 
     }
--- src/dataprocessing.h
+++ src/dataprocessing.h
@@ -5,10 +5,6 @@
 #include <unordered_map>
 #include <vector> 
 
-
-/**
- * TODO: Log sensor Data in SD card 
-*/
 /**
  * Will represent is a sensor is working or failing. If a sensor is failing, it is 
  * in fail mode, and we will display "FAILURE" for that sensor. 
@@ -17,19 +13,13 @@ typedef enum WorkingState {WORKING, FAILURE} WorkingState;
 /**
  * Flight Mode State
 */
-typedef enum FlightMode {StartupMode, StandbyMode, AscentMode,DrogueDeployedMode, MainDeployedMode,FaultMode} FlightMode; 
+typedef enum FlightMode {StartupMode, StandbyMode, AscentMode,DrogueDeployedMode, MainDeployedMode,FaultMode, Armed} FlightMode; 
 
 /**
  * The Class that stores the static member function that will read serial data, and then
  * u pdate the serialValues map with said data for the GUI's components to draw accurately. 
 */
 class SerialRead {
-    private: 
-        /**
-         * Byte array that we will have to unpack 
-        */
-       static std::byte package[56]; 
-
     public: 
          /**
          * Will represent the states of each sensor, six in total
@@ -40,6 +30,7 @@ class SerialRead {
         static WorkingState SDCardState; 
         static WorkingState AccelerometerState;
         static WorkingState TemperatureState;
+        static WorkingState RadioState; 
 
         static FlightMode FlightState;
 
@@ -54,3 +45,52 @@ class SerialRead {
         static void readPack();  
 
 }; 
+
+
+/**
+ * 6 bytes: Metadata
+3 bits: flight mode
+10 bits: Sensor statuses (2 bits each)
+1 bit: SD state
+1 bit: key armed
+1 bit: altitude armed
+4 bytes: timestamp
+3 bytes: Events
+(Will come up with enumeration)
+52 bytes: Sensor readings (4 bytes each)
+Altitude
+Latitude
+Longitude
+Elevation
+Accel X
+Accel Y
+Accel Z
+Gyro X
+Gyro Y
+Gyro Z
+Mag X
+Mag Y
+Mag Z 
+
+For the EVENTS:
+0: Key armed
+1: Altitude armed
+2: Alt init failed
+3: Alt reading failed
+4: Alt turned off
+5: IMU init failed
+6: IMU reading failed
+7: IMU turned off
+8: Accel init failed
+9: Accel reading failed
+10: Accel turned off
+11: Therm init failed
+12: Therm reading failed
+13: Therm turned off
+14: SD init failed
+15: SD write failed
+16: RFM init failed
+17: RFM transmit failed
+
+0 means false, 1 means true 
+*/
